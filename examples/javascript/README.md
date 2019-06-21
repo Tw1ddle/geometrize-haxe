05d692ee25c551cb212460a0c348133889930cd5
Sebastián Gurín <sgurin@awalabs.com>
2019-06-21 05:17:44 -0300

    browser example

---
+++ examples/javascript/README.md
@@ -0,0 +1,50 @@
+## Requirements
+
+You will need haxe and node.js installed in your system.
+
+## Install dependencies
+
+```sh
+npm install --save png.js
+npm install --save-dev browserify http-server terser
+```
+
+## Build
+
+Build JavaScript target:
+
+```sh
+haxe build-js-target.hxml 
+```
+
+This will generate a JavaScript version of the library at `build/geometrize.js`.
+
+Bundle browser example:
+
+```
+nxp browserify examples/javascript/browser-example.js -o build/browser-example.js 
+```
+
+This will generate `build/browser-example.js` which includes browser example plus all its dependencies, like geometrize, png.js, etc. See how it's used by [browser-example.html](browser-example.html)
+
+Minify bundle (optional):
+
+```sh
+npx terser build/browser-example.js -o build/browser-example.js 
+```
+
+Serve and open the example:
+
+```sh
+npx http-server . & 
+firefox http://localhost:8080/examples/javascript/browser-example.html &
+```
+
+### All together
+
+```sh
+haxe build-js-target.hxml 
+nxp browserify examples/javascript/browser-example.js -o build/browser-example.js 
+npx terser build/browser-example.js -o build/browser-example.js 
+npx http-server . 
+```
--- examples/javascript/browser-example.js
+++ examples/javascript/browser-example.js
@@ -1,14 +1,15 @@
-const { geometrize } = require('../../build/lib')
-var PNGReader = require('png.js')
+const { geometrize } = require('../../build/geometrize')
+const PNGReader = require('png.js')
 
 function loadPng(url) {
   return new Promise((resolve, reject) => {
     fetch(url).then(response =>
       response.arrayBuffer()
     ).then(bytes => {
-      var reader = new PNGReader(bytes)
+      const reader = new PNGReader(bytes)
       reader.parse(function (err, png) {
         if (err) reject(err)
+        document.getElementById('inputSize').innerHTML = (png.pixels.length / 1000) + 'KB'
         const bitmap = geometrize.bitmap.Bitmap.createFromRawBytes(png.width, png.height, png.pixels)
         resolve(bitmap)
       })
@@ -16,16 +17,21 @@ function loadPng(url) {
   })
 }
 
-async function test() {
-  document.getElementById('run').addEventListener('click', e=>{
-    const imgUrl = '/docs/logo.png'
-    const iterations = parseInt(document.getElementById('iterations').value||'500')
-    const interval = parseInt(document.getElementById('interval').value||'0')
-    run({imgUrl, iterations, interval})
-  })  
+function renderSvg(bitmap, svgData) {
+  const svg = geometrize.exporter.SvgExporter.getSvgPrelude() + geometrize.exporter.SvgExporter.getSvgNodeOpen(bitmap.width, bitmap.height) + svgData.join('\n') + geometrize.exporter.SvgExporter.getSvgNodeClose()
+  document.getElementById('output').innerHTML = svg
+  document.getElementById('outputSize').innerHTML = (svg.length / 1000) + 'KB'
 }
 
-async function run({imgUrl,iterations, interval}) {
+function wait(ms) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve()
+    }, ms)
+  })
+}
+
+async function run({ imgUrl, iterations, interval }) {
   const bitmap = await loadPng(imgUrl)
   const runner = new geometrize.runner.ImageRunner(bitmap)
   const options = {
@@ -37,7 +43,7 @@ async function run({imgUrl,iterations, interval}) {
   const svgData = []
   for (let i = 0; i < iterations; i++) {
     svgData.push(geometrize.exporter.SvgExporter.exportShapes(runner.step(options)))
-    if(interval){
+    if (interval) {
       renderSvg(bitmap, svgData)
       await wait(interval)
     }
@@ -45,17 +51,47 @@ async function run({imgUrl,iterations, interval}) {
   renderSvg(bitmap, svgData)
 }
 
-function renderSvg(bitmap, svgData) {
-  const svg = geometrize.exporter.SvgExporter.getSvgPrelude() + geometrize.exporter.SvgExporter.getSvgNodeOpen(bitmap.width, bitmap.height) + svgData.join('\n') + geometrize.exporter.SvgExporter.getSvgNodeClose();
-  document.getElementById('output').innerHTML = svg;
-}
-
-function wait(ms) {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve()
-    }, ms)
+async function test() {
+  document.getElementById('main').innerHTML = html
+  document.getElementById('run').addEventListener('click', async e => {
+    const imgUrl = document.getElementById('imgUrl').value || 'logo.png'
+    const iterations = parseInt(document.getElementById('iterations').value || '100')
+    const interval = parseInt(document.getElementById('interval').value || '0')
+    document.getElementById('status').innerHTML = 'Working'
+    const t0 = Date.now()
+    await run({ imgUrl, iterations, interval })
+    document.getElementById('elapsedTime').innerHTML = ((Date.now() - t0) / 1000) + 'seconds'
+    document.getElementById('status').innerHTML = 'Stand by'
+  })
+  document.getElementById('imgUrl').addEventListener('change', async e => {
+    document.getElementById('img').src = document.getElementById('imgUrl').value
   })
 }
+
+const html = `
+<img id="img" src="logo.png"/><br/>
+<div>Input size: <span id="inputSize"></span></div>
+<label>Image URL: <input type="url" value="logo.png" id="imgUrl"/></label><br/>
+<label>Iterations: <input id="iterations" type="number" min="1" value="100"/></label><br/>
+<label>Interval: <input id="interval" type="number" min="0" value="0"/></label><br/>
+<button id="run">run</button><br/>
+<div>Status: <span id="status">Stand by</span></div>
+<div>Output size: <span id="outputSize"></span></div>
+<div>Elapsed time: <span id="elapsedTime"></span></div>
+<div id="output"></div>
+<h4>Working Image URLs</h4>
+<p>png.js, the library to decode images that this example uses, support a limited set of formats (this is not a geometrize limitation but png.js'). This is a list of images that currently work for you to test:</p>
+<ul>
+<li>https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png</li>
+<li>logo.png</li>
+<li>https://upload.wikimedia.org/wikipedia/commons/b/b8/Lokazzjoni_ta%27_Ruma.png</li>
+<li>https://upload.wikimedia.org/wikipedia/commons/1/15/Xochiquetzal_V.png</li>
+<li>https://i.imgur.com/PzdDHyo.png?2</li>
+<li>https://i.imgur.com/8UQhIQQ.png</li>
+<li>https://upload.wikimedia.org/wikipedia/commons/8/86/Nikon_D3000_CN-2011-04.png</li>
+<li>https://upload.wikimedia.org/wikipedia/commons/d/d6/Wikipedia-logo-v2-en.png</li>
+<li>https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Fiji_banded_iguana_in_Vienna_Zoo_on_2013-05-12.png/640px-Fiji_banded_iguana_in_Vienna_Zoo_on_2013-05-12.png</li>
+</ul>
+`
 
 test()
